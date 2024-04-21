8cf4eb4de15c235479f781f3e6ac0af616c2b52d
svidlak <svid320@gmail.com>
2024-04-21 20:12:00 +0300

    Test

--- app/server.go
+++ app/server.go
@@ -159,7 +159,7 @@ func (rs *RedisServer) parseMessage(message []byte) string {
 				response := fmt.Sprintf("*%d\r\n", 3)
 				response += fmt.Sprintf("$%d\r\n%s\r\n", len(role), role)
 				response += fmt.Sprintf("$%d\r\n%s\r\n", len(masterId), masterId)
-				response += fmt.Sprintf("$%d\r\n%s", len(masterReplOffset), masterReplOffset)
+				response += fmt.Sprintf("$%d\r\n%s\r\n", len(masterReplOffset), masterReplOffset)
 
 				fmt.Print(response)
 				return response
